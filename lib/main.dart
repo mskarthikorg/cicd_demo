afe8bdaf1c22d726934edfd3c79c8b450d64144f
M S Karthik <ms.karthik@codepoints.in>
2026-09-01 13:04:37 +0530

    Format Dart code

--- lib/main.dart
+++ lib/main.dart
@@ -37,11 +37,9 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-
   //test collabrorator main
 
   //test collaborator main test
-
 
   @override
   Widget build(BuildContext context) {
